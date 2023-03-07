d3db55067df7eba7e74ec020eeb7fa6be95aa11d
Ryan <ryan.carpio@hotmail.com>
2023-03-07 09:30:03 +0100

    feat(MainActivity): declared variables and initialized them

--- app/src/main/java/zli/rc/coinview/MainActivity.java
+++ app/src/main/java/zli/rc/coinview/MainActivity.java
@@ -1,14 +1,27 @@
 package zli.rc.coinview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText searchInput;
+    private RecyclerView cryptosRV;
+    private ProgressBar loadingPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchInput = findViewById(R.id.idEditSearch);
+        cryptosRV = findViewById(R.id.idRVPriceCource);
+        loadingPB = findViewById(R.id.idLoadingBar);
+
     }
 }
