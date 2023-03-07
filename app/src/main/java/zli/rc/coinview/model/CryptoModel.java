054d8f6b7457074eaf586d04a89104158c6d9be0
Ryan <ryan.carpio@hotmail.com>
2023-03-07 15:52:47 +0100

    fix(cryptomodel): removed lombok annotations because it didnt really quite work

--- app/src/main/java/zli/rc/coinview/model/CryptoModel.java
+++ app/src/main/java/zli/rc/coinview/model/CryptoModel.java
@@ -3,12 +3,47 @@ package zli.rc.coinview.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+
 public class CryptoModel {
     private String name;
     private String symbol;
     private double price;
+
+    public CryptoModel(String name, String symbol, double price) {
+        this.name = name;
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 
 }
