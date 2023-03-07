d4e68adfe87f96838e5d3b412905391f714c24b3
Ryan <ryan.carpio@hotmail.com>
2023-03-07 09:51:50 +0100

    feat(build.gradle and cryptomodel): implemented new dependecy for lombok and created cryptomodel

---
+++ app/src/main/java/zli/rc/coinview/model/CryptoModel.java
@@ -0,0 +1,15 @@
+package zli.rc.coinview.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CryptoModel {
+    private String name;
+    private String symbol;
+    private double price;
+
+
+}
